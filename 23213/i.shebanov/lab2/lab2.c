7229ec5e1ac0b6ab2433856fc9808986c7df8277
Ivan Shebanov <i.shebanov@g.nsu.ru>
2024-09-09 16:44:48 +0700

    fixes

--- 23213/i.shebanov/lab2/lab2.c
+++ 23213/i.shebanov/lab2/lab2.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
 int main() {
     setenv("TZ", "America/Los_Angeles", 1);
 
     time_t current_time;
-    time(&current_time);
+    current_time = time(NULL);
+
     printf(asctime(localtime(&current_time)));
 
-    return 0;
+    exit(0);
 }
